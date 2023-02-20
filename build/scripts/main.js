0f3fe672d083813fd1aa41f5157d319ae3a202dc
Konstantin Podenezhniy <67436313+kpoden@users.noreply.github.com>
2023-02-20 16:44:01 +0500

    prices and banner done

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -4,10 +4,10 @@
 //     contain: true
 //   });
 
-var swiper = new Swiper(".orts-slider", {
+var mainSlider = new Swiper(".orts-slider", {
     spaceBetween: 30,
     pagination: {
-        el: ".swiper-pagination",
+        el: '.main-slider-pagination',
         clickable: true,
         },
     // autoplay: {
@@ -16,3 +16,22 @@ var swiper = new Swiper(".orts-slider", {
     //     pauseOnMouseEnter: true,
     //     }
 });
+
+
+const bannerCount = document.querySelectorAll('.orts-banner .swiper-wrapper .orts-banner-item').length;
+
+if(bannerCount > 1) {
+    var banner = new Swiper(".orts-banner", {
+    spaceBetween: 30,
+    pagination: {
+        el: ".banner-pagination",
+        clickable: true
+        },
+    // autoplay: {
+    //     delay: 3500,
+    //     disableOnInteraction: false,
+    //     pauseOnMouseEnter: true,
+    //     }
+});
+
+}
--- build/scripts/script.js
+++
@@ -1,8 +0,0 @@
-// $('.orts-slider').flickity({
-//     // options
-//     cellAlign: 'left',
-//     contain: true,
-//     autoPlay: 10000,
-//     freeScroll: true,
-//     wrapAround: true,
-// });

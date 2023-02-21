a6b047bcab11b36cbefe405a525b73d8397e5be7
Konstantin Podenezhniy <67436313+kpoden@users.noreply.github.com>
2023-02-21 18:50:20 +0500

    main page partly done  services started

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -37,3 +37,60 @@ if(bannerCount > 1) {
 }
 
 
+if(window.innerWidth < 700) {
+    document.querySelector('.swiper-wrapper-desktop').classList.add('swiper-wrapper');
+    document.querySelector('.reviews-spoiler').classList.add('hidden');
+
+    var reviews = new Swiper(".orts-reviews-clients", {
+    spaceBetween: 30,
+    pagination: {
+        el: ".reviews-pagination",
+        clickable: true
+        },
+    // autoplay: {
+    //     delay: 3500,
+    //     disableOnInteraction: false,
+    //     pauseOnMouseEnter: true,
+    //     }
+});
+
+}
+
+
+
+function hideReviews() {
+    const reviews =document.querySelectorAll('.orts-reviews-client');
+
+    if(reviews.length > 2 && window.innerWidth >= 700) {
+        document.querySelector('.reviews-spoiler').classList.remove('hidden');
+
+        for(let i = 2; i < reviews.length; i++) {
+            reviews[i].classList.add('hidden-review');
+        }
+    }
+}
+
+hideReviews();
+
+const spoiler = document.querySelector('.reviews-spoiler');
+const hiddenReviews = document.querySelectorAll('.hidden-review');
+
+spoiler.addEventListener("click", (event) => {
+  if (!spoiler.classList.contains("expanded")) {
+    hiddenReviews.forEach((review) => {
+      review.classList.remove("hidden-review");
+      review.classList.add("shown-review");
+      spoiler.classList.add("expanded");
+      spoiler.textContent = "Скрыть";
+    });
+  } else {
+    hiddenReviews.forEach((review) => {
+        review.classList.add("hidden-review");
+        review.classList.remove("shown-review");
+        spoiler.classList.remove("expanded");
+        spoiler.textContent = "Показать всё";
+      });
+  }
+});
+
+
